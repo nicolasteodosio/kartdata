d46db99227f5085f72a4ca9908bd9ad5965ee83e
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2019-01-09 15:03:33 -0200

    Criando função que prepara resultados

--- processar.py
+++ processar.py
@@ -10,5 +10,36 @@ def parse_file(caminho, arquivo):
     except Exception as e:
         raise e
 
+
+def prepara_resultados(completo=False, caminho='samples'):
+    file = open("{}/kartparsed.txt".format(caminho), 'r')
+    mapa = {}
+    voltas_dadas = []
+    for line in file:
+        valores = line.replace('\n', '').split('; ')
+        try:
+            if not completo:
+                if valores[4] == '4' and '4' in voltas_dadas:
+                    return mapa
+
+            if valores[1] not in mapa:
+                mapa[valores[1]] = {'nome': valores[3],
+                                    'voltas': [{'numero': valores[4],
+                                                'tempo': valores[5],
+                                                'horario': valores[0],
+                                                'media': valores[6]}],
+                                    }
+            else:
+                mapa[valores[1]]['voltas'].append({'numero': valores[4],
+                                                   'tempo': valores[5],
+                                                   'horario': valores[0],
+                                                   'media': valores[6]})
+            voltas_dadas.append(valores[4])
+        except Exception as e:
+            raise e
+    return mapa
+
+
 if __name__ == '__main__':
     parse_file(caminho='samples', arquivo='kart')
+    mapa = prepara_resultados()
